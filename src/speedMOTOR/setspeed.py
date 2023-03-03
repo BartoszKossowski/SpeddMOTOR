5ab865c2a84f6966d9e7005711d91e29e4169e2a
BartoszKossowski <bartosz.kossowski@MOSTY.PL>
2023-03-03 17:06:53 +0100

    last (maybe)

--- src/speedMOTOR/setspeed.py
+++ src/speedMOTOR/setspeed.py
@@ -47,13 +47,13 @@ class nfs:
             else:
                 self.value = float(value)
 
+        if RPM is None and Hz is None or RPM is True and Hz is True:
+            raise ValueError("You must select one of the units to be 'True' in the class arguments, example: Hz=True.")
+
         if RPM:
             self.mode = "RPM"
         if Hz:
             self.mode = "Hz"
-
-        if RPM is None and Hz is None:
-            raise ValueError("You must select one of the units to be 'True' in the class arguments, example: Hz=True.")
 
     def show(self):
         print(f"Wartość: {self.value} | Jednostka: {self.mode}")
@@ -96,7 +96,6 @@ class nfs:
 
             RPS = float(self.value / 60)
             time_RPS = float(1/RPS)
-            print(f"To jest ilość obrotów na sekundę: {RPS} | To jest czas jednego obrotu: {time_RPS}")
 
             if self.duration == "seconds":
                 if duration_time < time_RPS:  # jeżeli czas trwania jest krótszy niż czas dla jednego obrotu
@@ -107,13 +106,22 @@ class nfs:
                 if isinstance(duration_time, float):
                     raise TypeError("For the duration in minutes, you need to specify a value as int()")
                 self.set_time = duration_time * 60
+                print(f"Ustawiamy czas: {self.set_time} sekund")
 
             if self.duration == "hours":
                 if isinstance(duration_time, float):
                     raise TypeError("For the duration in hours, you need to specify a value as int()")
                 self.set_time = duration_time * 60 * 60
 
+            if self.duration == "infinity":
+                if duration_time is not None:
+                    raise SyntaxError("You must not use the 'Infinity' method and give the duration, the infinity is only one, it is not Marvel's movie")
+                self.set_time = 9999999999999999999  # You will not live to end this time
+
+            print(f"To jest ilość obrotów na sekundę: {RPS} | To jest czas jednego obrotu: {time_RPS} | Częstotliwość f = {round(1/time_RPS)} Hz | Powinno się wykonać z {self.set_time/time_RPS} obrotów")
+
             if float(time_RPS) <= 1.0:
+                print(f"Weszliśmy do pętli, wybrany duration to: {self.duration}")
 
                 """
                 Trzeba napisać własny stoper
@@ -124,22 +132,15 @@ class nfs:
                 actual_microsecond = round(datetime.now().microsecond / 1000)  # startowy czas w milisekundach, będzie zwiększany wraz z różnicą current_microseconds
                 time_RPS = time_RPS * 1000
                 R = 0
-                # print(f"Od tego zaczynamy: {actual_microsecond}")
                 while True:
                     # aktualny czas
                     current_second = datetime.now().second
                     current_microseconds = round(datetime.now().microsecond / 1000)
 
-                    # time.sleep(0.1)
-
                     # Czasem dochodzimy do prędkości około 30 tys. RPM, to nam daje 0,0002 sekundy na jeden obrót,
                     # trzeba zatem wykonać stoper dla milisekund (do trzech miejsc po przecinku),
                     # mikrosekundy podzielić przez 1000
                     # tysięczna sekundy jest całkiem stabilna, na niej będziemy opierać licznik
-                    # print(f"To są nasze mikrosekundy przerobione na milisekundy: \n"
-                    #       f"Dziesiętna sekundy: {round(current_microseconds / 100000)} \n"
-                    #       f"Setna sekundy: {round(current_microseconds / 10000)} \n"
-                    #       f"Tysięczna sekundy: {round(current_microseconds / 1000)}")
 
                     # licznik mikrosekund jest nam potrzebny do wydawania sygnałów w odpowiednim przedziale czasowym
                     # za dane wejściowe o tym, co ile ma zostać wykonany obrót daje nam: time_RPS
@@ -147,59 +148,69 @@ class nfs:
                     # żeby liczyć time_RPS pomnożymy go razy 1000 co da nam 0,1 * 1000 = 100
                     # w takim przypadku mamy obrót co 100 milisekund
 
-                    # print(f"To jest current: {current_microseconds} | to jest actual: {actual_microsecond} | to jest time RPS: {time_RPS}")
                     if current_microseconds < 50 and current_microseconds < actual_microsecond:
                         while True:
                             current_microseconds = round(datetime.now().microsecond / 1000)
                             score = time_RPS - abs(actual_microsecond - 1000)
-                            # print(f"To jest current: {current_microseconds} | A to jest różnica: {score}")
-                            # print("asd")
                             if current_microseconds > score:
-                                # print(f"Różnica: {score} | curent: {current_microseconds} | Aktualny: {actual_microsecond}")
                                 actual_microsecond = current_microseconds
                                 R += 1
-                                # print(f"To jest current: {current_microseconds} | to jest actual: {actual_microsecond} | W zerowaniu")
                                 break
 
-                    # print(float(current_microseconds - actual_microsecond))
-                    # print(time_RPS)
                     if float(current_microseconds - actual_microsecond) > time_RPS:
                         # print("Zwiększamy")
                         # print(90*"=")
                         actual_microsecond += time_RPS
                         R += 1
-                        # print(f"To jest current: {current_microseconds} | to jest actual: {actual_microsecond} | zwykły przeskok 400")
 
                     """
                     Poniżej wykonany stoper dla sekund
                     """
 
-                    # print(f"Aktualna sekunda: {current_second} | Goniąca sekunda: {actual_second} | Licznik sekund: {sekundy}")
                     # trzeba stworzyć licznik sekundowy
                     # i licznik podążający za aktualnym stanem sekundnika
                     if current_second > actual_second:
                         actual_second += 1
                         sekundy += 1  # to tutaj liczymy sekundy, które upłynęły (licznik nie resetuje się 60 - 0)
+                        # print(f"Leci test | sekunda: {sekundy}")
+
                     if current_second == 0 and current_second != actual_second:  # przy naszym opóźnieniu 0,1 sekundy 10 razy się dodaje więc trzeba było dać warunek logiczny
                         actual_second = 0
                         sekundy += 1  # to również jest interpretowane jako +1 sekunda
 
                     # to musi być na końcu
-                    if duration_time == sekundy:  # gdy zadany czas pracy zostanie osiągnięty
+                    if self.set_time == sekundy:  # gdy zadany czas pracy zostanie osiągnięty
                         print(f"Minęło {sekundy} sekund")
                         print(f"Wykonało się {R} obrotów")
                         break
 
             if float(time_RPS) > 1.0:
-                # print("zaczynamy teraz co innego")
+                print("zaczynamy teraz co innego")
                 pass
 
+        if self.mode == "Hz":
+            """
+                Hz
+                to długość impulsu w danym okresie
+                Okres
+                to czas w jakim wykonuje się pełny impuls
+                Jednoznacznie działa to jak liczenie obwodu koła
+            """
+            while True:
+                print(datetime.now().microsecond)
 
-list = [60, 100, 150, 200, 400, 600, 1000, 1500, 2000, 5000, 8000, 10000]
-for rpm in list:
-    print(90 * "=")
-    print(f"Powinno być: {(rpm/60) * 10}")
-    a = nfs(value=rpm, RPM=True)
-    for x in range(3):
-        a.turnover("seconds", duration_time=10)
+
+# list = [60, 100, 150, 200, 400, 600, 1000, 1500, 2000, 5000, 8000, 10000]
+# for rpm in list:
+#     print(90 * "=")
+#     print(f"Powinno być: {(rpm/60) * 10}")
+#     a = nfs(value=rpm, RPM=True)
+#     for x in range(3):
+#         a.turnover("seconds", duration_time=10)
+#
+
+
+a = nfs(value=5000000, RPM=True)
+a.turnover("minutes", duration_time=1)
+
 
